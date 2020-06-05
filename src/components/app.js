3167a50a7d33bc7ae4bcb0dd08e01d827a8407bc
HajaSC <hajasc@protonmail.com>
2020-06-05 02:18:09 -0400

    Adds Portfolio Items Class

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import moment from "moment";
+
+import PortfolioContainer from './portfolio/portfolio-container'
 
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>Haja's React App - YES!</h2>
+      <div className="app">
+        <h1>Haja's React Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+        <PortfolioContainer />
       </div>
     );
   }

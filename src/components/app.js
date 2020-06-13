9669ca881d9da8affe726638060518d095581867
HajaSC <hajasc@protonmail.com>
2020-06-13 18:07:05 -0400

    Adds menubar, filter features

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
-import PortfolioContainer from './portfolio/portfolio-container'
+import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from './navigation/navigation-container';
 
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
+        <NavigationContainer />
         <h1>Haja's React Portfolio</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContainer />

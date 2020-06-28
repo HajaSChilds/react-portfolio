8ad4d477cf93373a7ad02cd45a9cc3b659fc0865
HajaSC <hajasc@protonmail.com>
2020-06-28 04:44:35 -0400

    Adds Navigation SCSS, starts Flexbox Header Components

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -7,14 +7,29 @@ export default class NavigationComponent extends Component {
     }
 
     render() {
-        return(
-            <div>
-              <NavLink exact to="/">Home</NavLink>
-              <NavLink to ="/about-me">About</NavLink>
-              <NavLink to="/contact">Contact</NavLink>
-              <NavLink to="/blog">Blog</NavLink>
-              { true ? <NavLink to="/new-blog">Add Blog</NavLink> : null }
-             </div>
-            );
+        return (
+          <div className="nav-wrapper">
+            <div className="left-side">
+                <NavLink exact to="/" activeClassName="nav-link-active">
+                    Home
+                </NavLink>
+                <NavLink to="/about-me" activeClassName="nav-link-active">
+                    About
+                </NavLink>
+                <NavLink to="/contact" activeClassName="nav-link-active">
+                    Contact
+                </NavLink>
+                <NavLink to="/blog" activeClassName="nav-link-active">
+                    Blog
+                </NavLink>
+                {true ? (
+                    <NavLink to="/new-blog" activeClassName="nav-link-active">
+                    Add Blog
+                    </NavLink>
+                ) : null}
+            </div>
+            <div className="right-side">Haja Childs</div>
+          </div>
+        );
     }
 }

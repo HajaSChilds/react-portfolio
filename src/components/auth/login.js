1279082c00bf9157ee92a45dc796fe4a4f47b72d
HajaSC <hajasc@protonmail.com>
2020-07-09 23:24:48 -0400

    Completes login and error handling

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
  
 export default class Login extends Component {
     constructor(props) {
@@ -6,7 +7,8 @@ export default class Login extends Component {
 
         this.state = {
             email: "",
-            password: ""
+            password: "",
+            errorText: ""
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -15,12 +17,36 @@ export default class Login extends Component {
 
         handleChange(event) {
           this.setState({
-              [event.target.name]: event.target.value
+              [event.target.name]: event.target.value,
+              errorText: ""
           });
         }
     
         handleSubmit(event) {
-            console.log("Handle submit", event);
+            axios.post("https://api.devcamp.space/sessions", 
+            {
+              client: {
+                email:this.state.email,
+                password: this.state.password
+              }
+            },
+            { withCredentials: true }
+            ) .then(response => {
+              if (response.data.status == 'created') {
+                console.log("You can come in...")
+              }else {
+                this.setState({
+                  errorText: "Wrong email or password"
+                })
+              }
+            }).catch(error => {
+            
+              this.setState({
+                errorText: "An error occurred"
+              });
+            });
+          
+            event.preventDefault();
         }
     
     
@@ -28,7 +54,7 @@ export default class Login extends Component {
      return (
        <div>
          <h1>Login to Access Your Dashboard</h1>
-
+         <div>{this.state.errorText}</div>
          <form onSubmit={this.handleSubmit}>
            <input
              type="email"
@@ -46,7 +72,7 @@ export default class Login extends Component {
            />
 
            <div>
-               <button type="submit">Login</button>
+             <button type="submit">Login</button>
            </div>
          </form>
        </div>

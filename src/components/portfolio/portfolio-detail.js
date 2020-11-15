2c94299c762e836b41dd393bde6888897a280fdd
HajaSC <hajasc@protonmail.com>
2020-11-15 00:54:56 -0500

    Adds Portfolio Detail Page Basics

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+import axios from 'axios';
  
-export default function(props) {
-     return(
+export default class PortfolioDetail extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            currentId: this.props.match.params.slug,
+            portfolioItem: {}
+        }
+    }
+
+    componentDidMount(){
+        axios.get(`https://hajasc.devcamp.space/portfolio/portfolio_items/${this.state.currentId}` 
+      )
+      .then(response => {   
+          console.log("portfolio-detail-response:", response.data)
+          this.setState({portfolioItem: response.data.portfolio_item})       
+      })
+      .catch(error => {
+      console.log("detailImageError", error);
+    })
+    }
+  
+
+    render(){
+        const { name, description, banner_image_url } = this.state.portfolioItem
+      return(
          <div>
-           <h2>Portfolio Detail for {props.match.params.slug}</h2>
+           <img src={banner_image_url} alt=""/>
+           <h2>{name}</h2>
+           <p>{description}</p>
        </div>
-    );
+      );
+    }
 }
